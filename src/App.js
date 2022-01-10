de6266eca144483ec39f24f345cd03684badc1b6
Ahmadshah Wahidy <shah.wahidy@gmail.com>
2022-01-10 14:24:54 -0800

    Completed Module

--- src/App.js
+++ src/App.js
@@ -1,13 +1,73 @@
 import React from 'react';
+import ToDoForm from './components/TodoForm';
+import ToDoList from './components/TodoList';
 
+
+const tasks = [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+];
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+   constructor(){
+    super();
+    this.state ={
+      tasks: tasks
+    }
+  }
+
+  handleClearCompleted = () => {
+    this.setState({
+      ...this.state,
+      tasks: this.state.tasks.filter(item => {
+        return !item.completed;
+      })
+    })
+  }
+
+  handleAddTask = (taskName) => {
+    const newTask ={
+      name:taskName,
+      id: Date.now(),
+      completed: false
+    }
+    
+    this.setState({
+      ...this.state,
+      tasks: [...this.state.tasks, newTask]
+    });
+  }
+
+  handleToggleTask = (taskId) => {
+    this.setState({
+      ...this.state,
+      tasks: this.state.tasks.map(task => {
+        if(task.id === taskId.id) {
+          return({
+            ...task,
+            completed: !task.completed
+          })
+        } else {
+          return task;
+        }
+        
+      })
+    });
+  }
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <ToDoForm handleAddTask={this.handleAddTask}/>
+        <ToDoList handleToggleTask={this.handleToggleTask} tasks={this.state.tasks} />
+        <button onClick={this.handleClearCompleted}className="clear-btn">Clear Completed</button>
       </div>
     );
   }
---
+++ src/components/TodoForm.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+class ToDoForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ""
+    }
+  }
+
+  handleChanges = e => {
+    this.setState({
+      ...this.state,
+      input: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.handleAddTask(this.state.input);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input onChange={this.handleChanges} type="text" name="task" />
+        <button>Add</button>
+      </form>
+    );
+  }
+}
+
+export default ToDoForm;
